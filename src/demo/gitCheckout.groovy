f7ae5e2b806d22a8636f3c7158f03e910233d7ae
shrikantrshelke <shrikantrshelke@gmail.com>
2020-12-27 21:56:55 +0530

    Updted gm.groovy

--- src/demo/gitCheckout.groovy
+++ src/demo/gitCheckout.groovy
@@ -4,8 +4,8 @@ public class gitCheckout{
     def getCode(String REPO_NAME, String branch){
         checkout changelog: false, poll: false,
         scm: [$class: 'GitSCM',
-        branches: [[name: '*/'+ branch]],
+        branches: [[name: "*/${branch}"]],
         doGenerateSubmoduleConfigurations: false,
-        userRemoteConfigs: [[credentialsId: 'Github', url: REPO_NAME]]]
+        userRemoteConfigs: [[credentialsId: 'Github', url: "${REPO_NAME}"]]]
     }
 }
